ac5077bcaab267e3aeca6867b65f3907dee222c6
authdemo <62802604+authdemo@users.noreply.github.com>
2020-07-07 16:51:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ data "luminate_user" "users" {
   users                = [var.luminate_user]
 }
 
-//data "luminate_group" "groups" {
-  //identity_provider_id = data.luminate_identity_provider.idp.identity_provider_id
-  //groups               = [var.luminate_group]
-//}
+data "luminate_group" "groups" {
+  identity_provider_id = data.luminate_identity_provider.idp.identity_provider_id
+  groups               = [var.luminate_group]
+}
